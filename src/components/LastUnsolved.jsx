fbb40fe6bfdfe2b3e97617d7b88a904858baff54
HemanRamlal <namehubab@gmail.com>
2026-02-26 14:33:31 +0530

    fix: LastUnsolved component no longer ugly AND added 'open problem on click' for the list (fixes #2)

--- src/components/LastUnsolved.jsx
+++ src/components/LastUnsolved.jsx
@@ -1,6 +1,7 @@
 import "./LastUnsolved.css";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+import { useNavigate } from "react-router";
 import { getUserAtom } from "../atoms/user";
 import { useAtomValue } from "jotai";
 import { useQuery } from "@tanstack/react-query";
@@ -10,13 +11,17 @@ import LastUnsolvedFallback from "./fallbacks/LastUnsolvedFallback";
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo("en-US");
 function UnsolvedItem({ data }) {
+  const navigate = useNavigate();
+  function navigateToProblem() {
+    navigate(`/practice/${data.slug}`);
+  }
   return (
-    <div className="unsolved">
-      <div className="unsolved-since">{timeAgo.format(new Date(data.last_submission))}</div>
+    <div className="unsolved" onClick={navigateToProblem}>
       <div className="unsolved-problem">
         <div className="unsolved-problem-id">{data.pid}.</div>
         <div className="unsolved-problem-name">{data.title}</div>
       </div>
+      <div className="unsolved-since">{timeAgo.format(new Date(data.last_submission))}</div>
     </div>
   );
 }
@@ -43,10 +48,6 @@ export default function LastUnsolved() {
       )}
       {list.length != 0 && (
         <div className="lastUnsolved-list">
-          <div className="unsolved-header">
-            <div className="unsolved-since-header">Last Attempted</div>
-            <div className="unsolved-problem-header">Problem</div>
-          </div>
           {list && list.map(item => <UnsolvedItem key={item.submissionId} data={item} />)}
         </div>
       )}
