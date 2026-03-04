ed8200b26f4fd16a0fd31a5730e98bedeaa9b6e3
HemanRamlal <namehubab@gmail.com>
2026-03-04 15:43:56 +0530

    Footer redesigned for early release

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,14 @@ import { faYoutube, faReddit, faSquareXTwitter } from "@fortawesome/free-brands-
 import { faUserTie, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+function EarlyAccessBanner() {
+  return (
+    <div className="early-access-banner">
+      <h1>Early Access</h1>
+      <p>Still in development. Might be buggy</p>
+    </div>
+  );
+}
 function ContactLink({ link, icon, color }) {
   return (
     <motion.a
@@ -23,15 +31,7 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        {/*
-          <a href="/" className="link">
-            <WordMark className="wordmark-xl" />
-          </a> */}
-        <ContactLink link="https://www.youtube.com" icon={faYoutube} color="#ff0033" />
-        <ContactLink link="https://www.reddit.com" icon={faReddit} color="#ff4500" />
-        <ContactLink link="https://www.twitter.com" icon={faSquareXTwitter} color="#222222" />
-        <ContactLink link="mailto:example@example.com" icon={faEnvelope} color="#2255ff" />
-        <ContactLink link="https://www.linkedin.com/in/example" icon={faUserTie} color="#22ff55" />
+        <EarlyAccessBanner />
       </div>
     </>
   );
